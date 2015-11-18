b7ac37c646482e22ea8cb82f2d441c5e477b4157
Eloi Pardo <eloi.pardo.rivero@est.fib.upc.edu>
2015-11-18 19:05:30 +0000

    .

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,7 @@ class Comment < ActiveRecord::Base
     acts_as_votable
     belongs_to :submission
     has_many :replies
+    validates :content, presence: true
     after_initialize :set_default_values
     def set_default_values
         self.points ||= 0
