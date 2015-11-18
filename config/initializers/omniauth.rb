438364bc2716ab6fb6e6e43ff0872999af8847aa
carlosrive <carlos.rivero@est.fib.upc.edu>
2015-11-18 20:25:51 +0000

    prova heroku // variables de keys a

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :twitter, ENV['Z5te4Esq7QoleXt4vAcrDmezY'], 
-    ENV['okPnsy7m9W8S4cTzR92M8WmcVensBxGR2CVC1RyLOvTXzJVV2a']
+    provider :twitter, 'Z5te4Esq7QoleXt4vAcrDmezY', 
+    'okPnsy7m9W8S4cTzR92M8WmcVensBxGR2CVC1RyLOvTXzJVV2a'
 end
